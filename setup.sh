a2eb53b1e5b2df9a1df172a73f728041df093087
kangwooklee29 <kangwooklee29@gmail.com>
2024-02-09 22:54:47 +0900

    Change the script to use sleepwatcher

--- setup.sh
+++ setup.sh
@@ -2,17 +2,39 @@
 
 plist_file_name="com.kangwooklee29.btsunset.plist"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+check_program() {
+    program_name=$1
+    if ! command -v $program_name &>/dev/null; then
+        echo "$program_name is not installed. Please install it using brew."
+        echo "Run: brew install $program_name"
+        exit 1
+    fi
+}
+
+install_plist_file() {
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+    cp "${SCRIPT_DIR}/${plist_file_name}" ~/Library/LaunchAgents/
+
+    sed -i '' "s|YourUsername|${HOME}|g" "${HOME}/Library/LaunchAgents/${plist_file_name}"
+
+    launchctl unload ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
+    launchctl load ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
+}
+
+check_program blueutil
+check_program sleepwatcher
 
 mkdir -p ~/.btsunset
-touch ~/.btsunset/turn_off_time
-cp "${SCRIPT_DIR}/btsunset.sh" ~/.btsunset/
-cp "${SCRIPT_DIR}/${plist_file_name}" ~/Library/LaunchAgents/
 
-btsunset_file_path="${HOME}/.btsunset/btsunset.sh"
-sed -i '' "s|<string></string>|<string>${btsunset_file_path}</string>|g" "${HOME}/Library/LaunchAgents/${plist_file_name}"
+echo "/opt/homebrew/bin/blueutil -p 0" > ~/.btsunset/.sleep
+echo "/opt/homebrew/bin/blueutil -p 1" > ~/.btsunset/.wakeup
 
-launchctl unload ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
-launchctl load ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
+chmod +x ~/.btsunset/.sleep
+chmod +x ~/.btsunset/.wakeup
+
+cp btsunset.sh ~/.btsunset
+
+install_plist_file
 
 echo "installed BTSunset successfully!"
