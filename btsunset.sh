3c0999796226043c91119bad7e1a5f8790234420
kangwooklee29 <kangwooklee29@gmail.com>
2024-02-09 14:53:35 +0900

    Refactor script files

--- btsunset.sh
+++ btsunset.sh
@@ -1,59 +1,97 @@
 #!/bin/bash
 
-if [ $(( $(date +%s) - $(stat -f "%m" ~/.btsunset/turn_off_time) )) -le 10800 ]; then
-    # bluetooth was turned off in last 3 hours.
-    exit 0
-fi
-
-if ! command -v blueutil &> /dev/null; then
-    echo "blueutil is not installed. Please install it using brew."
-    echo "Run: brew install blueutil"
-fi
-
-airport_path=$(find /System/Library/PrivateFrameworks/ -name airport 2>/dev/null)
-
-for path in $airport_path; do
-    if $path -s &>/dev/null; then
-        airport=$path
-        break
-    fi
-done
-
-if [ -z "$airport" ]; then
-    echo "cannot find airport utility."
-    exit 1
-fi
-
-current_ap_list=$($airport -s | tail -n +2 | awk '{print $1}')
-
+# Global variables
+log_file_path="/tmp/com.kangwooklee29.btsunset.log"
 ap_list_path="$HOME/.btsunset/current_wifi_ap_list"
+turn_off_time_path="$HOME/.btsunset/turn_off_time"
 
-if [ -z "$ap_list_path" ]; then
-    echo $($airport -s | tail -n +2 | awk '{print $1}') >> $ap_list_path
-    exit 0
-fi
+# Logging function
+log_message() {
+    echo "$1" >> "$log_file_path"
+}
 
-previous_ap_list=$(cat "$ap_list_path")
-
-unique_flag=0
-for current_ap in $current_ap_list; do
-    if grep -q "$current_ap" <<< "$previous_ap_list"; then
-        # previous_ap_list has current_ap.
-        unique_flag=1
-        break
+# Check and trim log file
+check_trim_log() {
+    if [ $(wc -l < "$log_file_path") -gt 10 ]; then
+        sed -i '' '1,2d' "$log_file_path"
     fi
-done
+}
 
-# turn off bluetooth if the current ap list is totally different, as it means you left from the office.
-if [ $unique_flag -eq 0 ]; then 
-    blueutil -p 0
-    touch ~/.btsunset/turn_off_time
-fi
+# Checks Bluetooth turn-off time
+check_turn_off_time() {
+    if [ $(( $(date +%s) - $(stat -f "%m" "$turn_off_time_path" 2>/dev/null || echo 0) )) -le 10800 ]; then
+        log_message "Script ended, reason: 3 hours"
+        exit 0
+    fi
+}
 
-# update current_wifi_ap_list file to the latest.
-echo $($airport -s | tail -n +2 | awk '{print $1}') >> $ap_list_path
+# Check if blueutil is installed
+check_blueutil() {
+    if ! command -v blueutil &>/dev/null; then
+        echo "blueutil is not installed. Please install it using brew."
+        echo "Run: brew install blueutil"
+        log_message "Script ended, reason: no blueutil"
+        exit 1
+    fi
+}
 
-# delete first line of current_wifi_ap_list file if it has more than ten rows.
-if [ $(wc -l < "$ap_list_path") -gt 10 ]; then
-    sed -i '1d' "$ap_list_path"
-fi
+# Find and validate the Airport utility
+find_airport() {
+    airport_path=$(find /System/Library/PrivateFrameworks/ -name airport 2>/dev/null)
+    for path in $airport_path; do
+        if "$path" -s &>/dev/null; then
+            echo "$path"
+            return
+        fi
+    done
+    return 1
+}
+
+# Check if current AP list is different from the previous
+check_ap_list_difference() {
+    airport=$1
+    current_ap_list=$($airport -s | tail -n +2 | awk '{print $1}')
+    if [ ! -f "$ap_list_path" ]; then
+        echo "$current_ap_list" > "$ap_list_path"
+        log_message "Script ended, reason: Initialized AP list"
+        exit 0
+    fi
+
+    previous_ap_list=$(cat "$ap_list_path")
+
+    for current_ap in $current_ap_list; do
+        if grep -q -x "$current_ap" <<< "$previous_ap_list"; then
+            return 1 # Not Unique
+        fi
+    done
+
+    return 0 # Unique
+}
+
+# Main script logic
+main() {
+    check_trim_log
+    log_message "Script started at: $(date)"
+    check_turn_off_time
+    check_blueutil
+    airport=$(find_airport)
+    if [ $? -ne 0 ]; then
+        echo "cannot find airport utility."
+        log_message "Script ended, reason: no airport"
+        exit 1
+    fi
+
+    if check_ap_list_difference "$airport"; then
+        blueutil -p 0 # turn off bluetooth
+        touch "$turn_off_time_path"
+        echo $($airport -s | tail -n +2 | awk '{print $1}') > "$ap_list_path"
+        log_message "Bluetooth turned off due to new network."
+    else
+        log_message "Same network detected. Bluetooth remains on."
+    fi
+
+    log_message "Script ended, reason: script succeeded"
+}
+
+# Execute main function
+main
--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-launchctl unload ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
+plist_file_name="com.kangwooklee29.btsunset.plist"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 mkdir -p ~/.btsunset
-
-cp "${SCRIPT_DIR}/btsunset.sh" ~/.btsunset/
-
-cp "${SCRIPT_DIR}/com.kangwooklee29.btsunset.plist" ~/Library/LaunchAgents/
-
 touch ~/.btsunset/turn_off_time
+cp "${SCRIPT_DIR}/btsunset.sh" ~/.btsunset/
+cp "${SCRIPT_DIR}/${plist_file_name}" ~/Library/LaunchAgents/
 
+btsunset_file_path="${HOME}/.btsunset/btsunset.sh"
+sed -i '' "s|<string></string>|<string>${btsunset_file_path}</string>|g" "${HOME}/Library/LaunchAgents/${plist_file_name}"
+
+launchctl unload ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
 launchctl load ~/Library/LaunchAgents/com.kangwooklee29.btsunset.plist
 
 echo "installed BTSunset successfully!"
